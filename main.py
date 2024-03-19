cc68aa10b5ed4d0e1961708511c90db4988e795a
CHARLES CLARK <2clarc87@solent.ac.uk>
2024-03-19 11:33:45 +0000

    just better

--- main.py
+++ main.py
@@ -1,19 +1,2 @@
-<<<<<<< HEAD
-=======
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('jackson')
-
-
->>>>>>> 25617a64af8f2d79041e7e64a1301634bb4cc062
+from vehicle import *
+from car import *
